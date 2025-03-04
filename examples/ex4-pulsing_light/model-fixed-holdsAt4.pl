51dcc854378d3e6872fb8abafed14521cd8225d4
ivasicek <ivasicek@fit.vut.cz>
2025-03-04 16:38:16 +0100

    ex4 simplified

--- examples/ex4-pulsing_light/model-fixed-holdsAt4.pl
+++ examples/ex4-pulsing_light/model-fixed-holdsAt4.pl
@@ -7,67 +7,54 @@
 #show initiates/3, terminates/3, releases/3.
 #show trajectory/4.
 
-max_time(100).              % it is useful to have an upper bound on time for the full axioms
 
 
 % ----- domain model -----
 
 event(turn_light_on).       % start the fading process
-event(turn_light_off).      % stop the fading process
-event(switch_fade_in).      % start brightening from zero to max
-event(switch_fade_out).     % start dimming from max to zero
+event(switch_fade_out).     % stop fading in and start fading out (dimming)
+event(switch_fade_in).      % stop fading out (dimming) and starting fading in
 
-
+fluent(brightness(X)).      % range 0-10
 fluent(fading_in).
 fluent(fading_out).
-fluent(brightness(X)).      % range 0-10
-fluent(light_on).
 
-
-initiates(turn_light_on, light_on, T).
 initiates(turn_light_on, fading_in, T).
 releases(turn_light_on, brightness(X), T).
 
-terminates(turn_light_off, light_on, T).
-terminates(turn_light_off, fading_in, T) :- holdsAt(fading_in, T).
-terminates(turn_light_off, fading_out, T) :- holdsAt(fading_out, T).
-initiates(turn_light_off, light_intensity(0), T).
-terminates(turn_light_off, light_intensity(X), T) :- X .<>. 0.
-
-initiates(switch_fade_in, fading_in, T).
-terminates(switch_fade_in, fading_out, T).
-
-initiates(switch_fade_out, fading_out, T).
 terminates(switch_fade_out, fading_in, T).
+initiates(switch_fade_out, fading_out, T).
+
+terminates(switch_fade_in, fading_out, T).
+initiates(switch_fade_in, fading_in, T).
 
 
-trajectory(fading_in, T1, brightness(NewLevel), T2) :-
-    NewLevel .=. OldLevel + ((T2-T1) * 1),
-    holdsAt(brightness(OldLevel), T1).
+trajectory(fading_in, T1, brightness(NewB), T2) :-
+    NewB .=. OldB + ((T2-T1) * 1),
+    holdsAt(brightness(OldB), T1).
 
-trajectory(fading_out, T1, brightness(NewLevel), T2) :-
-    NewLevel .=. OldLevel - ((T2-T1) * 1),
-    holdsAt(brightness(OldLevel), T1).
+trajectory(fading_out, T1, brightness(NewB), T2) :-
+    NewB .=. OldB - ((T2-T1) * 1),
+    holdsAt(brightness(OldB), T1).
 
 
 epsilon(10).
 happens(switch_fade_out, T) :- !spy,
     epsilon(EPS),
-    holdsAt(light_on, T),
     holdsAt(brightness(10), T, fading_in, EPS).
 
 happens(switch_fade_in, T) :- !spy,
-    holdsAt(light_on, T),
+    epsilon(EPS),
     holdsAt(brightness(0), T, fading_out, EPS).
 
 
 % ----- narrative & queries  -----
 
+max_time(45). % set lower due to the cyclic behavior
 initiallyP(brightness(0)).
 initiallyN(F) :- not initiallyP(F).
 
 happens(turn_light_on, 10).
-happens(turn_light_off, 45).
 
 ?- holdsAt(brightness(X),   10).    % 0
 
@@ -82,6 +69,7 @@ happens(turn_light_off, 45).
 
 ?- happens(switch_fade_out, T).     % 20, 40
 ?- happens(switch_fade_in,  T).     % 30
+
 
 /* ----------------- MOVE THIS UP AND DOWN TO CHANGE QUERY ----------------- -/
 
