79fbf888557f37d1f353e83ca73f5f1e4118b274
ivasicek <ivasicek@fit.vut.cz>
2025-03-06 11:00:33 +0100

    ex6 blinking light

---
+++ examples/ex6-blinking_light/model-fixed-holdsAt3.pl
@@ -0,0 +1,55 @@
+#include './preprocessed_can_rules/model-fixed-holdsAt3-preprocessed.pl'. % include the can_* rules
+#show happens/2, not_happens/2.
+#show holdsAt/2, not_holdsAt/2.
+#show initiallyP/1, initiallyN/1.
+#show stoppedIn/3, not_stoppedIn/3.
+#show startedIn/3, not_startedIn/3.
+#show initiates/3, terminates/3, releases/3.
+#show trajectory/4.
+
+
+
+
+% ----- domain model -----
+
+event(turn_light_on).
+event(turn_light_off).
+
+fluent(light_on).
+
+initiates(turn_light_on,  light_on, T).
+terminates(turn_light_off, light_on, T).
+
+delay(1).
+happens(turn_light_off, T) :- !spy,
+    delay(D),
+    holdsAt(light_on, T, D).
+
+happens(turn_light_on, T) :- !spy,
+    delay(D),
+    not_holdsAt(light_on, T, D).
+
+
+% ----- narrative & queries  -----
+
+max_time(20). % set lower due to the cyclic behavior
+% initiallyN(light_on). % no initial value means no triggers until the first event
+happens(turn_light_on, 10).
+
+?-     holdsAt(light_on,    5).    % no models
+?- not_holdsAt(light_on,    5).    % no models
+
+?-     holdsAt(light_on,    15).   % yes
+?- not_holdsAt(light_on,    15).   % no models
+?-     holdsAt(light_on,    16).   % no models
+?- not_holdsAt(light_on,    16).   % yes
+
+?-     holdsAt(light_on,    T).    % (10,11], (12,13], (14,15], (16,17], (18,19]
+?- not_holdsAt(light_on,    T).    % (11,12], (13,14], (15,16], (17,18], (19,20]
+?- happens(turn_light_off,  T).    % 11, 13, 15, 17, 19
+?- happens(turn_light_on,   T).    % 10, 12, 14, 16, 18, 20
+
+
+/* ----------------- MOVE THIS UP AND DOWN TO CHANGE QUERY ----------------- -/
+
+/* ------------------------------ end of file ------------------------------ */
---
+++ examples/ex6-blinking_light/model.pl
@@ -0,0 +1,48 @@
+#include './preprocessed_can_rules/model-preprocessed.pl'. % include the can_* rules
+#show happens/2, not_happens/2.
+#show holdsAt/2, not_holdsAt/2.
+#show initiallyP/1, initiallyN/1.
+#show stoppedIn/3, not_stoppedIn/3.
+#show startedIn/3, not_startedIn/3.
+#show initiates/3, terminates/3, releases/3.
+#show trajectory/4.
+
+max_time(100).                      % it is useful to have an upper bound on time for the full axioms
+
+
+% ----- domain model -----
+
+event(turn_light_on).
+event(turn_light_off).
+
+fluent(light_on).
+
+initiates(turn_light_on,  light_on, T).
+terminates(turn_light_off, light_on, T).
+
+happens(turn_light_off, T) :- !spy,
+
+    holdsAt(light_on, T).
+
+happens(turn_light_on, T) :- !spy,
+    not_holdsAt(light_on, T).
+
+
+% ----- narrative & queries  -----
+
+% initiallyN(light_on). % no initial value means no triggers until the first event
+happens(turn_light_on, 10).
+
+?-     holdsAt(light_on,    5).    % non-term., should be no models
+?- not_holdsAt(light_on,    5).    % non-term., should be no models
+
+?-     holdsAt(light_on,    15).   % non-term., should be ??
+?- not_holdsAt(light_on,    15).   % non-term., should be ??
+
+?- happens(turn_light_off,  T).    % non-term., should be ??
+?- happens(turn_light_on,   T).    % non-term., should be 10 and ??
+
+
+/* ----------------- MOVE THIS UP AND DOWN TO CHANGE QUERY ----------------- -/
+
+/* ------------------------------ end of file ------------------------------ */
